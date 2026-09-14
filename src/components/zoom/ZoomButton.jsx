21eb45fa9e5ab32c3876f112513667720e98c4ed
Patricia-LF <patricia_lf_86@hotmail.com>
2026-09-14 20:02:40 +0200

    Create ZoomButton without content

---
+++ src/components/zoom/ZoomButton.jsx
@@ -0,0 +1,5 @@
+import styles from "./ZoomButton.module.css";
+import zoomBtnLight from "../../assets/icons/zoom-btn-light.png";
+import zoomBtnDark from "../../assets/icons/zoom-btn.png";
+
+export default function ZoomButton() {}
