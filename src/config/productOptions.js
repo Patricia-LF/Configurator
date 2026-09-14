a4d4daba1d933e8b3d728c2f9b8bcc87e5f3606e
Patricia-LF <patricia_lf_86@hotmail.com>
2026-09-14 15:11:20 +0200

    Add price to options

--- src/config/productOptions.js
+++ src/config/productOptions.js
@@ -6,16 +6,19 @@ export const woodVeneerOptions = [
     label: "Walnut",
     value: "Walnut",
     background: "url('/textures/walnut.jpg')",
+    price: 0,
   },
   {
     label: "Maple",
     value: "Maple",
     background: "url('/textures/maple.jpg')",
+    price: 0,
   },
   {
     label: "Mahogany",
     value: "Mahogany",
     background: "url('/textures/mahogany.jpg')",
+    price: 0,
   },
 ];
 
@@ -24,18 +27,24 @@ export const legMaterialBaseOptions = [
     label: "Silver",
     value: "Silver",
     background: "url('/textures/silver.png')",
+    price: 0,
   },
-  { label: "Gold", value: "Gold", background: "url('/textures/gold.png')" },
+  {
+    label: "Gold",
+    value: "Gold",
+    background: "url('/textures/gold.png')",
+    price: 0,
+  },
 ];
 
 export const productOptions = {
   size: {
     length: {
       label: "Length",
-      default: "118cm",
+      default: "200cm",
       options: [
-        { label: "118cm", value: "118cm" },
-        { label: "200cm", value: "200cm" },
+        { label: "200cm", value: "200cm", price: 26000 },
+        { label: "118cm", value: "118cm", price: 20000 },
       ],
     },
   },
@@ -45,8 +54,8 @@ export const productOptions = {
       label: "Add",
       default: "Yes",
       options: [
-        { label: "No", value: "No" },
-        { label: "Yes", value: "Yes" },
+        { label: "Yes", value: "Yes", price: 8000 },
+        { label: "No", value: "No", price: 0 },
       ],
     },
 
@@ -54,8 +63,9 @@ export const productOptions = {
       label: "Speaker Grille",
       default: "Dark",
       options: [
-        { label: "Light", value: "Light" },
-        { label: "Dark", value: "Dark" },
+        { label: "Dark", value: "Dark", price: 0 },
+        { label: "Light", value: "Light", price: 0 },
+        { label: "No Fabric", value: "No Fabric", price: 0 },
       ],
     },
   },
@@ -64,8 +74,8 @@ export const productOptions = {
     surface: {
       label: "Cabinet Surface",
       options: [
-        { label: "Smooth", value: "Smooth" },
-        { label: "Textured", value: "Textured" },
+        { label: "Smooth", value: "Smooth", price: 0 },
+        { label: "Textured", value: "Textured", price: 2000 },
       ],
     },
     woodVeneer: {
@@ -84,20 +94,20 @@ export const productOptions = {
     baseColor: {
       label: "Base Color",
       options: [
-        { label: "White", value: "White", background: "#FFFBF8" },
-        { label: "Black", value: "Black", background: "#211E1E" },
-        { label: "Blue", value: "Blue", background: "#243E94" },
-        { label: "Orange", value: "Orange", background: "#CC4C15" },
-        { label: "Green", value: "Green", background: "#52582A" },
-        { label: "Red", value: "Red", background: "#641717" },
+        { label: "White", value: "White", background: "#FFFBF8", price: 0 },
+        { label: "Black", value: "Black", background: "#211E1E", price: 0 },
+        { label: "Blue", value: "Blue", background: "#243E94", price: 0 },
+        { label: "Orange", value: "Orange", background: "#CC4C15", price: 0 },
+        { label: "Green", value: "Green", background: "#52582A", price: 0 },
+        { label: "Red", value: "Red", background: "#641717", price: 0 },
       ],
     },
 
     basePlatter: {
       label: "Base Platter",
       options: [
-        { label: "Basic", value: "Basic" },
-        { label: "Acrylic", value: "Acrylic" },
+        { label: "Basic", value: "Basic", price: 0 },
+        { label: "Acrylic", value: "Acrylic", price: 500 },
       ],
     },
   },
